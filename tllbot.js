d27e026dfa403439225067bf0b560ba821592901
jacob24068 <30918688+jacob24068@users.noreply.github.com>
2017-12-15 20:46:18 -0600

    Create tllbot.js

---
+++ tllbot.js
@@ -0,0 +1,77 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+    client.user.setGame(`Use !Help for help.`);
+  });
+
+client.on("message", async message => {
+    if(message.author.bot && message.content.match(`Welcome to TLL! We hope you enjoy your stay.`)) return message.delete(2000)
+    if(message.author.bot) return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (message.channel == `391409706477813771`) return message.delete(1000)
+
+    if(message.content.indexOf(config.prefix) !== 0) return;
+
+    if (command === "verify" && message.channel == `391409706477813771`) {
+        message.reply(`Welcome to TLL! We hope you enjoy your stay.`)
+        member.addRole(message.guild.roles.find("name", "Verified")).catch(console.error);
+    }
+    
+
+    if(command === "kick") {
+      if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
+      
+      let member = message.mentions.members.first();
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.kickable) 
+        return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+
+      let reason = args.slice(1).join(' ');
+      if(!reason)
+        return message.reply("Please indicate a reason for the kick!");
+      
+      await member.kick(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+      message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+  
+    }
+    
+    if(command === "ban") {
+      if(!message.member.roles.some(r=>["Administrator"].includes(r.name)) )
+        return message.reply("Sorry, you don't have permissions to use this!");
+      
+      let member = message.mentions.members.first();
+      if(!member)
+        return message.reply("Please mention a valid member of this server");
+      if(!member.bannable) 
+        return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+  
+      let reason = args.slice(1).join(' ');
+      if(!reason)
+        return message.reply("Please indicate a reason for the ban!");
+      
+      await member.ban(reason)
+        .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+      message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+    }
+    
+    if(command === "purge") {
+      const deleteCount = parseInt(args[0], 10);
+      
+      if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+        return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+
+      const fetched = await message.channel.fetchMessages({count: deleteCount});
+      message.channel.bulkDelete(fetched)
+        .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+    }
+  });
+  
+  client.login(process.env.BOT_TOKEN);
