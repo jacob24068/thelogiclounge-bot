a5b1052db2de2ed91109b695edf05a973d108b1a
jacob24068 <30918688+jacob24068@users.noreply.github.com>
2017-12-19 13:49:24 -0600

    Update tllbot.js

--- tllbot.js
+++ tllbot.js
@@ -208,12 +208,12 @@ client.on("message", async message => {
       const member = message.mentions.members.first()
       let number = args.slice(0).join(' ');
       if (member) {
-        message.channel.send(`${member.displayName} is in ${ordinal_suffix_of(newT[member.id])} place, with ${saveData[member.id].toLocaleString()} points.`)
+        message.channel.send(`In ${ordinal_suffix_of(newT[member.id])} place, ${member.displayName}, with ${saveData[member.id].toLocaleString()} points.`)
       }else if (number) {
         const a = arr.length
         if (arr[a - number]) {
           let mem = message.guild.members.get(arr[a-number])
-          message.channel.send(`${ordinal_suffix_of(number)} place is ${mem.displayName}, with ${saveData[mem.id].toLocaleString} points.`)
+          message.channel.send(` ${mem.displayName} is in ${ordinal_suffix_of(number)} place is, with ${saveData[mem.id].toLocaleString()} points.`)
         }else return message.channel.send(`There is no person in ${ordinal_suffix_of(number)} place.`)
       }else{
       const a = arr.length - 1
