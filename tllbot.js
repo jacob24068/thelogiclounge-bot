cc6f84d1bc454cdaea848ed4884142c909744705
jacob24068 <30918688+jacob24068@users.noreply.github.com>
2017-12-20 16:46:35 -0600

    Update tllbot.js

--- tllbot.js
+++ tllbot.js
@@ -173,10 +173,10 @@ client.on("message", async message => {
         return message.reply("Sorry, you don't have permissions to use this!");
 
       const deleteCount = parseInt(args[0], 10);
-      
+ 
+      console.log(deleteCount)
       if(!deleteCount || deleteCount < 2 || deleteCount > 100)
         return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-
       const fetched = await message.channel.fetchMessages({count: deleteCount});
       message.channel.bulkDelete(fetched)
         .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
